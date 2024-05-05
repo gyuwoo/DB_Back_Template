c3536133860b9fcd1b969081fdfbefc39b0821c4
HeoGwan <20191610@g.dongseo.ac.kr>
2024-05-05 23:00:45 +0900

    feat : modifying signin, signup api

--- src/api/user/user.controller.js
+++ src/api/user/user.controller.js
@@ -12,13 +12,59 @@ export default [
         middleware: [],
         controller: async (req, res, next) => {
             try {
+                // 콘솔 메시지를 이용하여 개발자에게 어떤 컨트롤러로 진입했는지 알려준다
+                // (에러 발생 시 마지막 접근 API를 알기 위함)
+                console.log('[User SignUp Controller Enter]');
                 const UserServiceInstance = Container.get(UserService);
-                const data = await UserServiceInstance.SignUp();
+                const userInfo = req.body;
+                const data = await UserServiceInstance.SignUp(userInfo);
 
-                return res.status(200).json(data);
+                /**
+                 * 4091: 회원가입 성공
+                 * 4092: 이메일 없음
+                 * 4093: 비밀번호 없음
+                 * 4094: 회원 존재
+                 */
+                let message = '';
+                switch (data.status) {
+                    case 4091:
+                        message = '회원가입 성공';
+                        break;
+                    case 4092:
+                        message = '이메일 없음';
+                        break;
+                    case 4093:
+                        message = '비밀번호 없음';
+                        break;
+                    case 4094:
+                        message = '회원 존재';
+                        break;
+                    default:
+                        message = '알 수 없는 오류 발생!';
+                        break;
+                }
+
+                /*
+                    결과에 따른 메시지를 같이 보내 프론트에서 어떤 에러가 났는지 확실하게 알 수 있게 해준다.
+                    아래와 같이 응답을 보내면 프론트에선 다음과 같은 데이터를 전달받는다. (회원가입 성공 했다고 가정)
+                    {
+                        message: '회원가입 성공',
+                        status: 4091,
+                        data: {
+                            email: '1234',
+                            pw: '1234
+                        }
+                    }
+                */
+                return res.status(200).json({
+                    message,
+                    ...data,
+                });
             } catch (err) {
+                // 에러가 났을 경우를 대비하여 try-catch문을 이용하여 확실하게 에러 처리를 한다.
                 return res.status(500).json({
-                    data: 'error!'
+                    message: err,
+                    ...data,
                 })
             }
         }
@@ -29,17 +75,44 @@ export default [
      */
     {
         path: '/user/signin',
-        method: 'get',
+        method: 'post',
         middleware: [],
         controller: async (req, res, next) => {
             try {
+                console.log('[User SignIn Controller Enter]');
                 const UserServiceInstance = Container.get(UserService);
-                const data = await UserServiceInstance.SignIn();
+                const userInfo = req.body;
+                const data = await UserServiceInstance.SignIn(userInfo);
 
-                return res.status(200).json(data);
+                /**
+                 * 4091: 로그인 성공
+                 * 4092: 회원이 존재하지 않음
+                 * 4093: 비밀번호 틀림
+                 */
+                let message = '';
+                switch (data.status) {
+                    case 4091:
+                        message = '로그인 성공';
+                        break;
+                    case 4092:
+                        message = '회원이 존재하지 않음';
+                        break;
+                    case 4093:
+                        message = '비밀번호 틀림';
+                        break;
+                    default:
+                        message = '알 수 없는 오류 발생!';
+                        break;
+                }
+
+                return res.status(200).json({
+                    message,
+                    ...data,
+                });
             } catch (err) {
                 return res.status(500).json({
-                    data: 'error!'
+                    message: err,
+                    ...data
                 })
             }
         }
--- src/api/user/user.service.js
+++ src/api/user/user.service.js
@@ -8,25 +8,53 @@ export default class UserService {
     /**
      * 회원가입
      */
-    async SignUp() {
+    async SignUp(userInfo) {
         try {
+            const returnData = {
+                status: 4095,
+                data: null,
+            };
+
+            // 구조 분해 할당을 이용하여 원하는 변수만 추출하여 사용할 수 있다.
+            const { email, pw } = userInfo;
+
+            // 프론트에서 값이 제대로 넘어오지 않을 수 있기 때문에 이에 관한
+            // 에러 처리를 해준다.
+            if (!email) {
+                // 임이의 값을 정하여 에러 결과를 반환한다.
+                returnData.status = 4092;
+                return returnData;
+            }
+
+            if (!pw) {
+                returnData.status = 4093;
+                return returnData;
+            }
+
             const testUser = await models.user.findOne({
                 where: {
-                    id: 1,
+                    // 키 값과 변수 명이 같으므로 아래와 같이 콜론 없이도 사용할 수 있다.
+                    // email
+                    email: email,
                 }
             });
 
             if (!testUser) {
-                await models.user.create({
-                    email: 'test',
-                    pw: 'test'
+                const user = await models.user.create({
+                    email: email,
+                    pw: pw
                 });
 
-                return '회원가입 성공';
+                returnData.status = 4091;
+                returnData.data = user;
+                return returnData;
             }
 
-            return '회원 존재';
+            returnData.status = 4094;
+            return returnData;
         } catch (err) {
+            // 콘솔 메시지를 이용하여 개발자에게 어디에서 오류가 났는지 알려준다
+            console.log('[User] SignUp Service Error!' + err);
             throw err;
         }
     }
@@ -34,11 +62,39 @@ export default class UserService {
     /**
      * 로그인
      */
-    async SignIn() {
-        return await models.user.findOne({
-            where: {
-                email: 'test'
+    async SignIn(userInfo) {
+        try {
+            const returnData = {
+                status: 4095,
+                data: null,
+            };
+
+            const { email, pw } = userInfo;
+
+            const user = await models.user.findOne({
+                where: {
+                    // 변수 명과 키 값이 같기 때문에 email: email을 아래와 같이 간단하게 작성 가능하다.
+                    email,
+                }
+            });
+
+            if (!user) {
+                returnData.status = 4092;
+                return returnData;
             }
-        });
+
+            if (user.pw != pw) {
+                returnData.status = 4093;
+                return returnData;
+            }
+
+            returnData.status = 4091;
+            returnData.data = user;
+
+            return returnData;
+        } catch (err) {
+            console.log('[User] SignIn Service Error!' + err);
+            throw err;
+        }
     }
 }
